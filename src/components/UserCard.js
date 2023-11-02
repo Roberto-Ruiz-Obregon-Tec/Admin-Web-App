dc868d4e90dfc327f5def42320e44abc6af12309
OsvaldoDM <a01275702@tec.mx>
2023-11-02 11:47:00 -0600

    Add: mostrar rol en cards

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -3,11 +3,12 @@ import '../styles/UserCard.css';
 
 function UserCard(props) {
   const { user } = props;
-  const { firstName, lastName, email, _id } = user;
+  const { firstName, lastName, email, rol, _id } = user;
 
   return (
       <div className='card-container'>
           <h3 className='name'>{firstName} {lastName}</h3>          
+          <p className='rol'> {rol} </p>
           <p>{email}</p>
       </div>
   );
