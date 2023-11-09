fa921b87376605190c9bf49a9ece8abfbfa4a9da
RodrigoTeran <rodrigo.teranhernandez@gmail.com>
2023-11-09 13:33:57 -0600

    feat: creado el componente de return link

--- src/screens/CreateProjects.js
+++ src/screens/CreateProjects.js
@@ -1,30 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import BtnToAdd from "../components/AddAdminContentBtn";
-
-import { getProgram } from '../client/availableProj'
-import { FireError } from '../utils/alertHandler';
+import React from 'react';
+import ReturnLink from "../components/ReturnLink";
 import '../styles/verCursos.css';
-import '../styles/availableProj.css'
 import '../styles/wrappers/wrap.css';
 
 const CreateProjects = () => {
 
-    const [avaliableP, setavailableP] = useState ([]);
-
-    // useEffect(() => {
-    //     (async () => {
-    //         try {
-    //             const proj = await getProgram();
-    //             setavailableP(proj);
-    //         } catch (error) {
-    //             FireError(error.response.message);
-    //         }
-    //     })();
-    // }, []);
-
     return (
         <div className='container_page_wrapper'>
-            Hola :)
+            <div className='header-container'>
+                <h4>Inicio / Proyectos Disponibles / Crear Proyecto</h4>
+            </div>
+            <ReturnLink href="/consultprojects" />
         </div>
     );
 };
