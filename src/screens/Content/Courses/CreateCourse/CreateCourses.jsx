6819318fc3af943f6a62cc6c69a24a0c79fc20e0
RodrigoTeran <rodrigo.teranhernandez@gmail.com>
2023-11-13 10:13:11 -0600

    feat: added dropdown component

--- src/screens/Content/Courses/CreateCourse/CreateCourses.jsx
+++ src/screens/Content/Courses/CreateCourse/CreateCourses.jsx
@@ -5,6 +5,7 @@ import { createPublications } from "../../../../client/publications";
 import NavHistory from "../../../../components/NavHistory/NavHistory";
 import Card from "../../../../components/ShadowCard/ShadowCard";
 import { PATH_COURSES } from "../../../../config/paths";
+import DropDown from "../../../../components/Form/Input/DropDown/DropDown";
 
 import InputText from "../../../../components/Form/Input/Text/Text";
 import InputTextArea from "../../../../components/Form/Input/TextArea/TextArea";
@@ -18,8 +19,26 @@ const CreateCourses = () => {
     const navigate = useNavigate();
     const [isLoading, setIsLoading] = useState(false);
 
-    const [title, setTitle] = useState('');
-    const [description, setDescription] = useState('');
+    const [name, setName] = useState("");
+    const [description, setDescription] = useState("");
+    const [speaker, setSpeaker] = useState("");
+    const [startDate, setStartDate] = useState("");
+    const [endDate, setEndDate] = useState("");
+    const [schedule, setSchedule] = useState("");
+
+    const [modality, setModality] = useState("");
+    const [isOpenmodality, setIsOpenModality] = useState(false);
+
+    const [postalCode, setPostalCode] = useState("");
+    const [location, setLocation] = useState("");
+    const [status, setStatus] = useState("");
+    const [cost, setCost] = useState("");
+    const [courseImage, setCourseImage] = useState("");
+    const [capacity, setCapacity] = useState("");
+    const [rating, setRating] = useState("");
+    const [ratingCount, setRatingCount] = useState("");
+    const [meetingCode, setMeetingCode] = useState("");
+    const [accessCode, setAccessCode] = useState("");
 
     const [file, setFile] = useState(null);
 
@@ -30,38 +49,38 @@ const CreateCourses = () => {
      */
     const handleSubmit = async (e) => {
         e.preventDefault();
-        if (
-            title.trim() === "" ||
-            description.trim() === ""
-        ) {
-            FireError('No puedes dejar los campos vacíos.');
-            return;
-        }
-        if (file === null) {
-            FireError('Debes de subir una imagen');
-            return;
-        }
-        try {
-            const data = {
-                title,
-                description,
-                image: "https://ejemplo.com",
-            };
-            setIsLoading(true);
-            const response = await createPublications(data);
-            setIsLoading(false);
+        // if (
+        //     title.trim() === "" ||
+        //     description.trim() === ""
+        // ) {
+        //     FireError('No puedes dejar los campos vacíos.');
+        //     return;
+        // }
+        // if (file === null) {
+        //     FireError('Debes de subir una imagen');
+        //     return;
+        // }
+        // try {
+        //     const data = {
+        //         title,
+        //         description,
+        //         image: "https://ejemplo.com",
+        //     };
+        //     setIsLoading(true);
+        //     const response = await createPublications(data);
+        //     setIsLoading(false);
 
-            if (response.status === 'success') {
-                FireSucess('Has creado una publicación exitosamente.');
-                navigate(PATH_COURSES);
-            } else {
-                FireError('Ha habido un error.');
-            }
-        } catch (error) {
-            setIsLoading(false);
-            if ([400, 401].includes(error.response.status)) FireError(error.response.data.message);
-            else FireError('Ocurrió un error. Por favor intenta de nuevo.');
-        }
+        //     if (response.status === 'success') {
+        //         FireSucess('Has creado una publicación exitosamente.');
+        //         navigate(PATH_COURSES);
+        //     } else {
+        //         FireError('Ha habido un error.');
+        //     }
+        // } catch (error) {
+        //     setIsLoading(false);
+        //     if ([400, 401].includes(error.response.status)) FireError(error.response.data.message);
+        //     else FireError('Ocurrió un error. Por favor intenta de nuevo.');
+        // }
     };
 
     // name
@@ -95,29 +114,49 @@ const CreateCourses = () => {
                             Crear curso
                         </h1>
                         <form onSubmit={handleSubmit} className={styles.form}>
-                            <InputText
-                                id="new-post-name"
-                                text="Título"
-                                value={title}
-                                setValue={setTitle}
-                            />
-                            <InputTextArea
-                                id="new-post-description"
-                                text="Descripción"
-                                value={description}
-                                setValue={setDescription}
-                                className={styles.textarea}
-                            />
-                            <InputImage
-                                id="image-post-new"
-                                setFile={(file) => {
-                                    setFile(file);
-                                }}
-                                file={file}
-                            />
-                            <Button isAnimationLoading isLoading={isLoading} type='submit'>
-                                Crear curso
-                            </Button>
+                            <div className={styles.top}>
+                                <div className={styles.left}>
+                                    <DropDown
+                                        text={modality === "" ? "Modalidad" : modality}
+                                        attrBtn=""
+                                        attrDivParent=""
+                                        isOpen={isOpenmodality}
+                                        id="dropdown-new-course-modality"
+                                        setIsOpen={setIsOpenModality}
+                                    >
+                                        {["Remoto", "Presencial"].map((possibility, index) => {
+                                            return (
+                                                <div onClick={() => {
+                                                    setModality(possibility);
+                                                    setIsOpenModality(false);
+                                                }} attr-css="element" key={index}>
+                                                    {possibility}
+                                                </div>
+                                            )
+                                        })}
+                                    </DropDown>
+                                </div>
+                                <div className={styles.right}>
+                                    <InputText
+                                        id="new-course-name"
+                                        text="Nombre"
+                                        value={name}
+                                        setValue={setName}
+                                    />
+                                    <InputImage
+                                        id="image-post-new"
+                                        setFile={(file) => {
+                                            setFile(file);
+                                        }}
+                                        file={file}
+                                    />
+                                </div>
+                            </div>
+                            <div className={styles.bottom}>
+                                <Button isAnimationLoading isLoading={isLoading} type='submit'>
+                                    Crear curso
+                                </Button>
+                            </div>
                         </form>
                     </Card>
                 </div>
