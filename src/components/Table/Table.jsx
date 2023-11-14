7da170e66404579320ffb518e0cffd55ebecf933
sjimenezbauer <sjimenezbauer@gmail.com>
2023-11-14 17:40:33 -0600

    Modificar Tabla y agregar Certificaciones

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,65 +1,111 @@
-import styles from "./Table.module.css";
+import React from 'react';
+import styles from './Table.module.css';
 
 function Table({
-    matrixData,
-    arrayHeaders,
+  matrixData,
+  arrayHeaders,
 
-    percentages = null,
-    clickOnCell = null
+  percentages = null,
+  clickOnCell = null,
+
+  handleEdit,
+  handleDelete,
 }) {
+  const getPercentage = (i = null) => {
+    if (percentages === null || i === null)
+      return 100 / arrayHeaders.length;
 
-    const getPercentage = (i = null) => {
-        if (percentages === null || i === null) return 100 / arrayHeaders.length;
+    return percentages[i];
+  };
 
-        return percentages[i];
+  const gotAClick = (i = null) => {
+    if (clickOnCell === null) return;
+
+    clickOnCell(i);
+  };
+
+  const handleEditClick = () => {
+    if (handleEdit) {
+      handleEdit();
     }
+  };
 
-    const gotAClick = (i = null) => {
-        if (clickOnCell === null) return;
-
-        clickOnCell(i);
+  const handleDeleteClick = () => {
+    if (handleDelete) {
+      handleDelete();
     }
+  };
 
-    return (
-        <div className={styles.table}>
-            <div className={styles.row}>
-                {arrayHeaders.map((header, i) => {
-                    return (
-                        <div style={{
-                            width: `${getPercentage(i)}%`
-                        }} key={i} className={styles.header}>
-                            {header}
-                        </div>
-                    )
-                })}
+  return (
+    <div className={styles.table}>
+      <div className={styles.row}>
+        {arrayHeaders.map((header, i) => {
+          return (
+            <div
+              style={{
+                width: `${getPercentage(i)}%`,
+              }}
+              key={i}
+              className={styles.header}
+            >
+              {header}
             </div>
-            {matrixData.length === 0 && (
-                <div className={styles.zero}>
-                    No hay ninguna entrada
-                </div>
-            )}
-
-            {matrixData.map((row, i) => {
-                return (
-                    <div key={i} className={styles.row} style={{
-                        backgroundColor: i % 2 == 0 ? "#FFF" : "#DDD"
-                    }}>
-                        {row.map((element, j) => {
-                            return (
-                                <div onClick={() => {
-                                    gotAClick(i);
-                                }} style={{
-                                    width: `${getPercentage(j)}%`,
-                                    cursor: clickOnCell === null ? "default" : "pointer"
-                                }} className={styles.element} key={`${i}-${j}`}>
-                                    {element}
-                                </div>
-                            )
-                        })}
-                    </div>
-                )
-            })}
+          );
+        })}
+        <div style={{ width: '20%' }} className={styles.header}>
+          Actions
         </div>
-    )
+      </div>
+      {matrixData.length === 0 && (
+        <div className={styles.zero}>No hay ninguna entrada</div>
+      )}
+
+      {matrixData.map((row, i) => {
+        return (
+          <div
+            key={i}
+            className={styles.row}
+            style={{
+              backgroundColor: i % 2 === 0 ? '#FFF' : '#DDD',
+            }}
+          >
+            {row.map((element, j) => {
+              return (
+                <div
+                  onClick={() => {
+                    gotAClick(i);
+                  }}
+                  style={{
+                    width: `${getPercentage(j)}%`,
+                    cursor:
+                      clickOnCell === null ? 'default' : 'pointer',
+                  }}
+                  className={styles.element}
+                  key={`${i}-${j}`}
+                >
+                  {element}
+                </div>
+              );
+            })}
+            <div style={{ width: '20%' }} className={styles.element}>
+              <button
+                className={styles.whitebutton}
+                onClick={() => handleEditClick(i)}
+              >
+                Edit
+              </button>
+              <button
+                className={styles.redbutton}
+                onClick={() => handleDeleteClick(i)}
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
+
 export default Table;
